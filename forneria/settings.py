da7e7d70f0b5be5dd2872c17473b0a881deb92cd
BlackYcee <nicolas.galleguillos14@inacapmail.cl>
2025-12-09 00:22:21 -0300

    mysql a SQLITE

--- forneria/settings.py
+++ forneria/settings.py
@@ -107,19 +107,11 @@ TEMPLATES = [
 # ==========================================
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'forneria',
-        'USER': 'root',
-        'PASSWORD': 'REACH',  
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 
 # ==========================================
